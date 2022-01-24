d55b5d9268fd1e95452f192a3a1e23e26ee8a404
sandeepdash1 <sandeep.dash152@gmail.com>
2022-01-24 12:17:31 +0530

    added order micro-service

---
+++ src/main/java/com/abc/orderservices/OrderservicesApplication.java
@@ -0,0 +1,13 @@
+package com.abc.orderservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderservicesApplication.class, args);
+	}
+
+}
